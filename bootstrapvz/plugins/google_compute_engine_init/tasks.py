ba5aba7dbacdea7d7ff2797ba17e75123a02b27c
cdakin <42942730+cdakin@users.noreply.github.com>
2024-05-23 13:45:43 +0100

    Removed obsolete google apt repo

--- bootstrapvz/plugins/google_compute_engine_init/tasks.py
+++ bootstrapvz/plugins/google_compute_engine_init/tasks.py
@@ -6,15 +6,6 @@ from bootstrapvz.common.tools import log_check_call
 import os
 
 
-class AddGoogleComputeEngineInitRepos(Task):
-    description = 'Adding Google Compute Engine Init repositories.'
-    phase = phases.preparation
-    predecessors = [apt.AddManifestSources]
-
-    @classmethod
-    def run(cls, info):
-        info.source_lists.add('google-cloud-compute', 'deb http://packages.cloud.google.com/apt google-compute-engine-%s main' % info.manifest.plugins['google_compute_engine_init'].get('repository_suffix', '{system.release}') )
-
 class InstallGoogleComputeEngineInitPackages(Task):
     description = 'Installing Google Compute Engine Init packages.'
     phase = phases.preparation
